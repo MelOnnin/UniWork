a8a238ae27da3d3627c96866bc7d3e22b72bc8b3
Mel_ <149074506+MelOnnin@users.noreply.github.com>
2026-02-25 13:13:04 +0200

    Update xor_cipher.c

--- xor_cipher.c
+++ xor_cipher.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 void xor_cipher(char *text) {
-int len = strlen(text);
-char key = 'K';
-for (int i = 0; i < len; i++) {
-// TODO: Приложете XOR операция върху text[i]
-    text[i]=text[i]^key;
-}
+  int len = strlen(text);
+  char key = 'K';
+  for (int i = 0; i < len; i++) {
+    // TODO: Приложете XOR операция върху text[i]
+    text[i] = text[i] ^ key;
+  }
 }
 // В main функцията добавете:
-int main()
-{
-char data[] = "Secret";
+int main() {
+  char data[] = "Secret";
 
-xor_cipher(data);
-printf("Encrypted: %s\n", data); // Внимание: Може да съдържа невидими символи!
+  xor_cipher(data);
+  printf("Encrypted: %s\n",
+         data); // Внимание: Може да съдържа невидими символи!
 
-xor_cipher(data);
-printf("Decrypted: %s\n", data);
+  xor_cipher(data);
+  printf("Decrypted: %s\n", data);
+
+  return EXIT_SUCCESS;
 }
