41529d3e564c62052bfce49e7f2e0c18ad3f60bd
Mel_ <149074506+MelOnnin@users.noreply.github.com>
2026-03-18 21:43:07 +0200

    Update frequency_analysis_caesar.c

--- frequency_analysis_caesar.c
+++ frequency_analysis_caesar.c
@@ -6,7 +6,6 @@ void print_histogram(const char *text) {
   int total_letters = 0;
   int biggest = 0;
 
-  // 1. Преброяване
   for (int i = 0; text[i] != '\0'; i++) {
     if (isalpha(text[i])) {
       int index = toupper(text[i]) - 'A';
@@ -14,17 +13,14 @@ void print_histogram(const char *text) {
       total_letters++;
     }
   }
-  // 1.5 Намира най-често срещаната буква
   for (int i = 0; i < 26; i++) {
     if (counts[i] > biggest)
       biggest = counts[i];
   }
-  // 2. Визуализация
   printf("\n--- FREQUENCY HISTOGRAM ---\n");
   for (int i = 0; i < 26; i++) {
     if (counts[i] > 0) {
       printf("%c (%3d): ", 'A' + i, counts[i]);
-      // Цикъл за печатане на звездички
       for (int j = 0; j < counts[i]; j++) {
         printf("*");
       }
@@ -34,7 +30,6 @@ void print_histogram(const char *text) {
       }
     }
   }
-  // 3. Принтира предположението за ключа на текста с декриптирания текст
   printf("Suggested keys: %d ,%d ,%d, %d", biggest - 'E',biggest - 'T',biggest - 'A',biggest - 'O');
 }
 
